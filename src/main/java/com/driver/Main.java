3c3df02da1af9b478dde81bf010cee98ae7fa0cb
Dinesh Kukreja <dhirensharma540@gmail.com>
2023-01-13 13:16:17 +0530

    updated source code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,31 +1,28 @@
 package com.driver;
 
 public class Main {
+
+    public static class Product{
+        public int product(int x,int y){
+            return x*y;
+        } 
+    
+        public int product(int x,int y,int z){
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
+
     public static void main(String[] args) {
         Product p = new Product();
-        int x=0,y=0,z=0;
-        double a=0,b=0;
+        int x=1,y=1,z=1;
+        Double a = 1.5, b = 2.5;
 
-        p.product(x,y);
-        p.product(x,y,z);
-        p.product(a, b);
-
-
-    }
-}
-
-class Product{
-    public int product(int x,int y){
-        // System.out.println("calling 1st product function");
-        return y;
-    } 
-
-    public int product(int x,int y,int z){
-        // System.out.println("calling 2nd product function");
-        return y;
-    }
-    public double product(double x, double y) {
-        // System.out.println("calling 3rd product function");
-        return y;
+        System.out.println(p.product(x,y));
+        System.out.println(p.product(x,y,z));
+        System.out.println(p.product(a, b));
+        
     }
 }
